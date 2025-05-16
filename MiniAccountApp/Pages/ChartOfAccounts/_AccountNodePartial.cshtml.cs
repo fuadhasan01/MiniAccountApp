d3e726be3d01dc5138791593c398bfd518b06ce6
Fuad Hasan <labib.knc@gmail.com>
2025-05-16 18:29:50 +0100

    Multi layer hierrarchy in coa

---
+++ MiniAccountApp/Pages/ChartOfAccounts/_AccountNodePartial.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MiniAccountApp.Pages.ChartOfAccounts
+{
+    public class _AccountNodePartialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
